da5c39edfa2c64a145a44f50f102947966c31ee0
unenglishable <unenglishable@gmail.com>
2014-07-17 18:04:37 -1000

    add example querier
    queries the test_table in the needless database

---
+++ exQuerier.js
@@ -0,0 +1,30 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'lavenderberry',
+  database: 'needless',
+  password: '_n4rcol3PSY'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.query('SHOW columns FROM test_table', function (err, rows, fields) {
+  if(err) throw err;
+
+  console.log('Columns from test_table:');
+  console.log(rows);
+});
+
+connection.query('SELECT * FROM test_table', function (err, rows, fields) {
+  if (err) throw err;
+
+  console.log('Query response: ', rows);
+});
+
+connection.end();
